f8da29643721d1e75afb064f3a6650725a80d7ec
Fernanda Pereira <ferps@br.ibm.com>
2018-10-08 10:40:10 -0300

    pattern changes
    changing methods to static

--- src/br/com/ibm/dynapool/pages/Login_page.java
+++ src/br/com/ibm/dynapool/pages/Login_page.java
@@ -2,10 +2,10 @@ package br.com.ibm.dynapool.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import br.com.ibm.dynapool.engine.Selenium_Engine;
+import static br.com.ibm.dynapool.engine.Selenium_Engine.*;
 
 
-public class Login_page {
+public class Login_page{
 
 	WebDriver driver;
 	By userName = By.name("txtUsername");
@@ -13,22 +13,22 @@ public class Login_page {
 	By GDPRcheckbox = By.name("chkAgreement");
 	By loginBtn = By.name("btnSubmit");
 
-	Selenium_Engine engine = new Selenium_Engine();
+
 
 	public void setUserName(String strUserName) {
 		driver.findElement(userName).sendKeys(strUserName);
 	}
 
 	public void setUserPassword(String strUserPassword) {
-		engine.setTextbox(userPassword, strUserPassword);
+		setTextbox(userPassword, strUserPassword);
 	}
 
 	public void setGDPRcheckbox(boolean enabled) {
-		engine.selectCheckBox(GDPRcheckbox);
+		selectCheckBox(GDPRcheckbox);
 	}
 	
 	public void clickLoginButton() {
-		engine.click(loginBtn);
+		click(loginBtn);
 	}
 
 }
