f4377ae0da14cf93871f8876a345dbd40d496a1d
Fernanda Pereira <ferps@br.ibm.com>
2018-10-08 16:31:09 -0300

    Master Task request - methods
    new methods addition

--- src/br/com/ibm/dynapool/pages/request/MasterTaskRequest_Page.java
+++ src/br/com/ibm/dynapool/pages/request/MasterTaskRequest_Page.java
@@ -1,5 +1,7 @@
 package br.com.ibm.dynapool.pages.request;
 
+import static br.com.ibm.dynapool.engine.Selenium_Engine.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -14,16 +16,75 @@ public class MasterTaskRequest_Page {
 	By complexityDpx = By.name("selDefcomplexity");
 	By summaryTxt = By.name("txtSummary");
 	By descriptionTxa = By.name("txaDescription");
-	By automatasCrl = By.name("crlAutomataAvailable");
-	By addAutomataCrl = By.cssSelector("input.crossListButton:nth-child(1)");//verify the better way
-	By removeAutomataCrl = By.cssSelector("input.crossListButton:nth-child(3)");//verify the better way
+//	By automatasAvailableCrl = By.name("crlAutomataAvailable");
+//	By addAutomataCrl = By.cssSelector("input.crossListButton:nth-child(1)");
+//	By removeAutomataCrl = By.cssSelector("input.crossListButton:nth-child(3)");
+//	By automatasSelectedCrl = By.name("crlAutomataSelected");
 	By taskSourceDpx = By.name("selDeftasksource");
 	By automataCategoryDpx = By.name("selDefautomatacategory");
 	By alert_ServerCountTxt = By.name("txtAutomatacount");
 	By defectCategoryDpx = By.name("selDefdefectcategory");
-	By sameBtn = By.name("btnSubmit");
+	By saveBtn = By.name("btnSubmit");
 	By clearBtn = By.name("btnReset");
 	By cancelBtn = By.name("btnCancel");
-	
-	
+
+	public void setCountry(String strCountry) {
+		selectDropDown(targetCountryDpx, strCountry);
+	}
+
+	public void setAccount(String strAccount) {
+		selectDropDown(targetAccountDpx, strAccount);
+	}
+
+	public void setEnvironment(String strEnvironment) {
+		selectDropDown(environmentDpx, strEnvironment);
+	}
+
+	public void setClass(String strClass) {
+		selectDropDown(classDpx, strClass);
+	}
+
+	public void setPriority(String strPriority) {
+		selectDropDown(priorityDpx, strPriority);
+	}
+
+	public void setComplexity(String strComplexity) {
+		selectDropDown(complexityDpx, strComplexity);
+	}
+
+	public void setSummary(String strSummary) {
+		setTextbox(summaryTxt, strSummary);
+	}
+
+	public void setDescription(String strDescription) {
+		setTextbox(descriptionTxa, strDescription);
+	}
+
+	public void setTaskSource(String strTaskSource) {
+		selectDropDown(taskSourceDpx, strTaskSource);
+	}
+
+	public void setAutomataCategory(String strAutomataCategory) {
+		selectDropDown(automataCategoryDpx, strAutomataCategory);
+	}
+
+	public void setAlert_ServerCount(String strAlert_ServerCount) {
+		setTextbox(alert_ServerCountTxt, strAlert_ServerCount);
+	}
+
+	public void setDefectCategory(String strDefectCategory) {
+		selectDropDown(defectCategoryDpx, strDefectCategory);
+	}
+
+	public void clickSaveButton() {
+		click(saveBtn);
+	}
+
+	public void clickClearButton() {
+		click(clearBtn);
+	}
+
+	public void clickCancelButton() {
+		click(cancelBtn);
+	}
 }
