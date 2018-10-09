a3b92a4ab619ec4649bf390c3d9b911d6151f3f5
Fernanda Pereira <ferps@br.ibm.com>
2018-10-09 18:25:59 -0300

    Extension Requests list - Mapping

---
+++ src/br/com/ibm/dynapool/pages/dispatching/ExtensionRequestsList_Page.java
@@ -0,0 +1,17 @@
+package br.com.ibm.dynapool.pages.dispatching;
+
+import org.openqa.selenium.By;
+
+public class ExtensionRequestsList_Page {
+	
+	By magnifierFlt = By.className("bigCardTitle");
+	By previousBtn = By.name("btnPrevious page");
+	By closeBtn = By.name("btnClose");
+	By nextBtn = By.name("btnNext page");
+	By idFlt = By.linkText("id");
+	By taskFlt = By.linkText("task");
+	By reasonFlt = By.linkText("reason");
+	By createdFlt = By.linkText("created");
+	By statusFlt = By.linkText("status");
+	
+}
