ce445ea4984d1a9026e0ba6428c0ee7cf2728f98
Fernanda Pereira <ferps@br.ibm.com>
2018-10-10 15:20:11 -0300

    Account List - Mapping and Methods
    Component mapping
    Methods added

--- src/br/com/ibm/dynapool/pages/administration/AccountList_Page.java
+++ src/br/com/ibm/dynapool/pages/administration/AccountList_Page.java
@@ -1,5 +1,69 @@
 package br.com.ibm.dynapool.pages.administration;
 
-public class AccountList_Page {
+import org.openqa.selenium.By;
 
+import br.com.ibm.dynapool.engine.Selenium_Engine;
+
+public class AccountList_Page {
+	By magnifierFlt = By.className("bigCardTitle");
+	By previousBtn = By.name("btnPrevious page");
+	By closeBtn = By.name("btnClose");
+	By nextBtn = By.name("btnNext page");
+	By idFlt = By.linkText("id");
+	By nameFlt = By.linkText("name");
+	By countryFlt = By.linkText("country");
+	By statusFlt = By.linkText("status");
+	By firstItemLnk = By.xpath("/html/body/div[1]/div[3]/div/div[3]/div[2]/table/tbody/tr[2]");
+
+	public void clickMagnifierFilter() {
+		Selenium_Engine.click(magnifierFlt);
+	}
+
+	public void clickPreviousButton() {
+		Selenium_Engine.click(previousBtn);
+	}
+
+	public void clickCloseButton() {
+		Selenium_Engine.click(closeBtn);
+	}
+
+	public void clickNextButton() {
+		Selenium_Engine.click(nextBtn);
+	}
+
+	public void clickIdFilter() {
+		Selenium_Engine.click(idFlt);
+	}
+
+	public void clickNameFilter() {
+		Selenium_Engine.click(nameFlt);
+	}
+
+	public void clickCountryFilter() {
+		Selenium_Engine.click(countryFlt);
+	}
+
+	public void clickStatusFilter() {
+		Selenium_Engine.click(statusFlt);
+	}
+
+	public void clickFirstItemLink() {
+		Selenium_Engine.click(firstItemLnk);
+	}
+
+	public void doubleClickIdFilter() {
+		Selenium_Engine.doubleClick(idFlt);
+	}
+
+	public void doubleClickNameFilter() {
+		Selenium_Engine.doubleClick(nameFlt);
+	}
+
+	public void doubleClickCountryFilter() {
+		Selenium_Engine.doubleClick(countryFlt);
+	}
+
+	public void doubleClickStatusFilter() {
+		Selenium_Engine.doubleClick(statusFlt);
+	}
 }
