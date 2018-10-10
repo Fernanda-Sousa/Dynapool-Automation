d172791183ffd33cf49e78535e9427f60cf4e9a5
Fernanda Pereira <ferps@br.ibm.com>
2018-10-10 07:42:28 -0300

    Extension Request List - Methods
    Click and doubleClick methods were added

--- src/br/com/ibm/dynapool/pages/dispatching/ExtensionRequestsList_Page.java
+++ src/br/com/ibm/dynapool/pages/dispatching/ExtensionRequestsList_Page.java
@@ -1,6 +1,12 @@
 package br.com.ibm.dynapool.pages.dispatching;
 
+import static br.com.ibm.dynapool.engine.Selenium_Engine.click;
+
+import javax.print.attribute.standard.MediaSize.Engineering;
+
 import org.openqa.selenium.By;
+
+import br.com.ibm.dynapool.engine.Selenium_Engine;
 
 public class ExtensionRequestsList_Page {
 	
@@ -14,4 +20,59 @@ public class ExtensionRequestsList_Page {
 	By createdFlt = By.linkText("created");
 	By statusFlt = By.linkText("status");
 	
+	public void clickMagnifierFilter() {
+		click(magnifierFlt);
+	}
+
+	public void clickPreviousButton() {
+		click(previousBtn);
+	}
+
+	public void clickCloseButton() {
+		click(closeBtn);
+	}
+
+	public void clickNextButton() {
+		click(nextBtn);
+	}
+
+	public void clickIdFilter() {
+		click(idFlt);
+	}
+
+	public void clickTaskFilter() {
+		click(taskFlt);
+	}
+
+	public void clickReasonFilter() {
+		click(reasonFlt);
+	}
+
+	public void clickCreatedFilter() {
+		click(createdFlt);
+	}
+
+	public void clickStatusFilter() {
+		click(statusFlt);
+	}
+
+	public void doubleClickIdFilter() {
+		Selenium_Engine.doubleClick(idFlt);
+	}
+
+	public void doubleClickTaskFilter() {
+		Selenium_Engine.doubleClick(taskFlt);
+	}
+
+	public void doubleClickReasonFilter() {
+		Selenium_Engine.doubleClick(reasonFlt);
+	}
+
+	public void doubleClickCreatedFilter() {
+		Selenium_Engine.doubleClick(createdFlt);
+	}
+
+	public void doubleClickStatusFilter() {
+		Selenium_Engine.doubleClick(statusFlt);
+	}
 }
