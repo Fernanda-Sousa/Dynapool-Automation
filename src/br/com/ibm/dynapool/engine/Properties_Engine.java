d6131a3a4ef8066136e760d543deffed67fc4b22
Maycon Douglas <mdss@br.ibm.com>
2018-10-11 06:16:56 -0300

    Information
    Minor Changes.

--- src/br/com/ibm/dynapool/engine/Properties_Engine.java
+++ src/br/com/ibm/dynapool/engine/Properties_Engine.java
@@ -1,3 +1,14 @@
+/*
+Created by: Fernanda Pereira (ferps@br.ibm.com)
+Date: 10-oct-2018
+
+Last update date: 10-oct-2018
+Last updated by: Fernanda pereira (ferps@br.ibm.com)
+
+Version: 1.0.0.0
+Description: 
+*/
+
 package br.com.ibm.dynapool.engine;
 
 import java.io.FileOutputStream;
