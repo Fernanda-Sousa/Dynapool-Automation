4340fb38dcf86f68a03848aee8d67c163352a86a
Fernanda Pereira <ferps@br.ibm.com>
2018-10-11 11:07:31 -0300

    Automata List - Mapping and Methods
    component mapping
    click and doubleClick methods were added

--- src/br/com/ibm/dynapool/pages/administration/AutomataList_Page.java
+++ src/br/com/ibm/dynapool/pages/administration/AutomataList_Page.java
@@ -2,7 +2,7 @@
 Created by: Fernanda Pereira (ferps@br.ibm.com)
 Date: 10-oct-2018
 
-Last update date: 10-oct-2018
+Last update date: 11-oct-2018
 Last updated by: Fernanda pereira (ferps@br.ibm.com)
 
 Version: 1.0.0.0
@@ -11,8 +11,68 @@ Description:
 
 package br.com.ibm.dynapool.pages.administration;
 
+import org.openqa.selenium.By;
+
+import br.com.ibm.dynapool.engine.Selenium_Engine;
 import br.com.ibm.dynapool.pages.Home_Page;
 
-public class AutomataList_Page extends Home_Page{
+public class AutomataList_Page extends Home_Page {
+	By magnifierFlt = By.className("bigCardTitle");
+	By previousBtn = By.name("btnPrevious page");
+	By newBtn = By.name("btnNew");
+	By closeBtn = By.name("btnClose");
+	By nextBtn = By.name("btnNext page");
+	By idFlt = By.linkText("id");
+	By nameFlt = By.linkText("name");
+	By environmentFlt = By.linkText("environment");
+	By firstItemLnk = By.xpath("/html/body/div[1]/div[3]/div/div[3]/div[2]/table/tbody/tr[2]");
+
+	public void clickMagnifierFilter() {
+		Selenium_Engine.click(magnifierFlt);
+	}
+
+	public void clickPreviousButton() {
+		Selenium_Engine.click(previousBtn);
+	}
+
+	public void clickNewButton() {
+		Selenium_Engine.click(newBtn);
+	}
+
+	public void clickCloseButton() {
+		Selenium_Engine.click(closeBtn);
+	}
+
+	public void clickNextButton() {
+		Selenium_Engine.click(nextBtn);
+	}
+
+	public void clickIdFilter() {
+		Selenium_Engine.click(idFlt);
+	}
+
+	public void clickNameFilter() {
+		Selenium_Engine.click(nameFlt);
+	}
+
+	public void clickEnvironmentFilter() {
+		Selenium_Engine.click(environmentFlt);
+	}
+
+	public void clickFirstItemLink() {
+		Selenium_Engine.click(firstItemLnk);
+	}
+
+	public void doubleClickIdFilter() {
+		Selenium_Engine.doubleClick(idFlt);
+	}
+
+	public void doubleClickNameFilter() {
+		Selenium_Engine.doubleClick(nameFlt);
+	}
+
+	public void doubleClickEnvironmentFilter() {
+		Selenium_Engine.doubleClick(environmentFlt);
+	}
 
 }
