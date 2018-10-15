0ff92136188a6dfdd8efd13cbfaf3ee5d9740429
Fernanda Pereira <ferps@br.ibm.com>
2018-10-15 11:03:47 -0300

    Update - Minor changes
    Last update date changed

--- src/br/com/ibm/dynapool/engine/Selenium_Engine.java
+++ src/br/com/ibm/dynapool/engine/Selenium_Engine.java
@@ -2,7 +2,7 @@
 Created by: Fernanda Pereira (ferps@br.ibm.com)
 Date: 10-oct-2018
 
-Last update date: 10-oct-2018
+Last update date: 15-oct-2018
 Last updated by: Fernanda pereira (ferps@br.ibm.com)
 
 Version: 1.0.0.0
