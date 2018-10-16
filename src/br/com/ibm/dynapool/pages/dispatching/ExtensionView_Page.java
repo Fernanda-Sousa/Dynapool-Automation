a435d23ab2b268cdace8f00f20ccbe1c8b127172
Fernanda Pereira <ferps@br.ibm.com>
2018-10-16 14:12:41 -0300

    Update - Extension View - Methods

--- src/br/com/ibm/dynapool/pages/dispatching/ExtensionView_Page.java
+++ src/br/com/ibm/dynapool/pages/dispatching/ExtensionView_Page.java
@@ -2,7 +2,7 @@
 Created by: Fernanda Pereira (ferps@br.ibm.com)
 Date: 15-oct-2018
 
-Last update date: 15-oct-2018
+Last update date: 16-oct-2018
 Last updated by: Fernanda pereira (ferps@br.ibm.com)
 
 Version: 1.0.0.0
@@ -14,7 +14,9 @@ package br.com.ibm.dynapool.pages.dispatching;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ExtensionView_Page {
+import br.com.ibm.dynapool.pages.Home_Page;
+
+public class ExtensionView_Page extends Home_Page {
 
 	WebDriver driver;
 
@@ -42,5 +44,89 @@ public class ExtensionView_Page {
 	By rejectBtn = By.name("btnReject");
 	By closeBtn = By.name("btnClose");
 	By deleteBtn = By.name("btnDelete");
+
+	public void clickEditTab() {
+		selEngine.click(editLnk);
+	}
+
+	public int getIntegerTaskId(By idTxt) {
+		return Integer.parseInt(driver.findElement(idTxt).getText());
+	}
+
+	public int getIntegerId(By opportunityId) {
+		return Integer.parseInt(driver.findElement(opportunityId).getText());
+	}
+
+	public boolean compareTaskId(String strTaskId) {
+		return selEngine.compareText(idTxt, strTaskId);
+	}
+
+	public boolean compareCountry(String strCountry) {
+		return selEngine.compareText(targetCountryDpx, strCountry);
+	}
+
+	public boolean compareAccount(String strAccount) {
+		return selEngine.compareText(targetAccountDpx, strAccount);
+	}
+
+	public boolean comparePriority(String strPriority) {
+		return selEngine.compareText(priorityDpx, strPriority);
+	}
+
+	public boolean compareOwner(String strOwner) {
+		return selEngine.compareText(ownerDpx, strOwner);
+	}
+
+	public boolean compareSummary(String strSummary) {
+		return selEngine.compareText(summaryTxt, strSummary);
+	}
+
+	public boolean compareDescription(String strDescription) {
+		return selEngine.compareText(descriptionTxa, strDescription);
+	}
+
+	public boolean compareDueDate(String strDueDate) {
+		return selEngine.compareText(dueDateDtf, strDueDate);
+	}
+
+	public boolean compareId(String strId) {
+		return selEngine.compareText(idTxt, strId);
+	}
+
+	public boolean compareCreatedDate(String strCreatedDate) {// MM/dd/yyyy hh:mm:ss
+		return selEngine.compareText(createdDtf, strCreatedDate);
+	}
+
+	public boolean compareReason(String strReason) {
+		return selEngine.compareText(reasonTxa, strReason);
+	}
+
+	public boolean compareSuggestedDate(String strSuggestedDate) {// MM/dd/yyyy
+		return selEngine.compareText(suggestedDateDtf, strSuggestedDate);
+	}
+
+	public boolean compareStatus(String strStatus) {
+		return selEngine.compareText(statusDpx, strStatus);
+	}
+
+	public boolean compareJustification(String strJustification) {
+		return selEngine.compareText(justificationTxa, strJustification);
+	}
+
+	public void clickApproveButton() {
+		selEngine.click(approveBtn);
+	}
+
+	public void clickRejectButton() {
+		selEngine.click(rejectBtn);
+	}
+
+	public void clickCloseButton() {
+		selEngine.click(closeBtn);
+	}
+
+	public void clickDeleteButton() {
+		selEngine.click(deleteBtn);
+	}
 
 }
